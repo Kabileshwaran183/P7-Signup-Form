c1ba7f94831726a6cb43109237fb9c7a715d71cc
Kabileshwaran183 <Kabileshwaran183@gmail.com>
2024-12-17 11:11:29 +0530

    Create Radio and Textarea
    Practice the radio and textarea inputs in forms

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,26 @@ export default function App() {
 
         <label htmlFor="password">Password:</label>
         <input id="password" defaultValue="password123" type="password" name="password" />
+        
+        <label htmlFor="description">Description:</label>
+        <textarea id="description" name="description" defaultValue="This is a description"></textarea>
 
+        <fieldset>
+          <legend>Employment Status:</legend>
+          <label>
+            <input type="radio" name="employmentStatus" value="unemployed" />
+            Unemployed
+        </label>
+          <label>
+            <input type="radio" name="employmentStatus" value="part-time" />
+            Part-time
+        </label>
+          <label>
+            <input type="radio" name="employmentStatus" value="full-time" />
+            Full-time
+        </label>
+        </fieldset>
+        
         <button>Submit</button>
 
       </form>
